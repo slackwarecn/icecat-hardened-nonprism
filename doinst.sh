5e937c1387d4c7b28f40f6e827aff233c92b6cf1
mdrights <linusyangcn@gmail.com>
2017-09-03 21:55:42 +0800

    update at 3rd Sept, night.

--- doinst.sh
+++ doinst.sh
@@ -2,13 +2,19 @@
 
 ## You should set your $HOME dir manually. I can't help to decide which user you are using :)
 
+ICVER="52.1.0"
 HOME="/home/live"
-INIDIR=$(find $HOME/.mozilla/icecat -type d -name "*.default")
+#INIDIR=$(find $HOME/.mozilla/icecat -type d -name "*.default")
+PROFDIR="$HOME/.mozilla"
 
-if [ -f $INIDIR/extensions.ini ]; then
-	cp $INIDIR/extensions.ini $INIDIR/extensions.ini.orig
+if [ ! -d $PROFDIR ]; then
+	mkdir $PROFDIR
 fi
 
-install -Dm644 /usr/lib64/icecat-52.1.0/defaults/extensions.ini $INIDIR/extensions.ini
-install -Dm644 /usr/lib64/icecat-52.1.0/defaults/user.js $INIDIR/user.js
+#if [ -f $INIDIR/extensions.ini ]; then
+#	cp $INIDIR/extensions.ini $INIDIR/extensions.ini.orig
+#fi
+
+install -Dm644 /usr/lib64/icecat-52.1.0/defaults/extensions.ini $PROFDIR/icecat/extensions.ini
+install -Dm644 /usr/lib64/icecat-52.1.0/defaults/user.js $PROFDIR/icecat/user.js
 
