c294b1236bf55e6791136b1073d6f7da9a7fd0bc
mdrights <linusyangcn@gmail.com>
2017-09-10 09:57:36 +0800

    Check. Remove the malware addons.

--- download-addons.sh
+++ download-addons.sh
@@ -2,15 +2,6 @@
 
 DOWNDIR=$(pwd)
 
-# Download the ublock add-on.
-UBLOCK=1.11.4
-SOURCE=("https://addons.cdn.mozilla.net/user-media/addons/607454/ublock_origin-${UBLOCK}-an+fx+sm+tb.xpi")
-/usr/bin/wget -c -P $DOWNDIR -t 5 $SOURCE || echo "This add-on may not be downloaded."
-
-# Download the User-Agent Switcher.
-SOURCE=("https://addons.mozilla.org/firefox/downloads/latest/user-agent-switcher-firefox/addon-578976-latest.xpi")
-/usr/bin/wget -c -P $DOWNDIR -t 5 $SOURCE || echo "This addon may not be downloaded successfully."
-
 
 # Download the noscript add-on.
 NS=5.0.5
@@ -29,16 +20,5 @@ SOURCE=("https://secure.informaction.com/download/releases/noscript-${NS}.xpi")
 #    install -Dm644 noscript-${NS}.xpi "${_extension_dest}.xpi"
 #  fi
 
-## Since Icecat ships with the following features, you don't need to download and enable them.
 
-# Download the Disable-WebRTC add-on.
-#SOURCE=("https://addons.mozilla.org/firefox/downloads/latest/happy-bonobo-disable-webrtc/addon-497366-latest.xpi")
-#/usr/bin/wget -c -P $DOWNDIR -t 5 $SOURCE || true; echo "This addon may not be downloaded successfully."
-#install -Dm755 "${SOURCE##*/}" $PKG/$DESTDIR/jid1-5Fs7iTLscUaZBgwr@jetpack.xpi
-
-# Download the no-resource-uri-leak add-on.
-#NRUL=1.1.0
-#SOURCE=("https://addons.cdn.mozilla.net/user-media/addons/706000/no_resource_uri_leak-${NRUL}-an+tb+sm+fx.xpi")
-#/usr/bin/wget -c -t 5 $SOURCE || true; echo "This addon may not be downloaded successfully."
-#install -Dm755 "${SOURCE##*/}" $PKG/$DESTDIR/@no-resource-uri-leak.xpi
 
